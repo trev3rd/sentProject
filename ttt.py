6977d1b03bb606b3fc5ccd1747a2e2d67a0cc2a6
trev3rd <trevionm17@gmail.com>
2016-05-02 16:05:12 -0400

    Added files via upload

---
+++ ttt.py
@@ -0,0 +1,15 @@
+from nltk.tokenize import sent_tokenize, word_tokenize
+
+f = open('plu.txt')
+raw = f.read()
+
+
+
+print(sent_tokenize(raw))
+print(word_tokenize(raw))
+print('number of sentences equal',len(sent_tokenize(raw)))
+print('number of words equal',len(word_tokenize(raw)))
+
+
+average =(len(word_tokenize(raw))/len(sent_tokenize(raw)))
+print('average word per senetnces eqauls',average)
