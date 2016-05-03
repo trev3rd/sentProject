ddf01a3d928a0c1b334bd6a2fe70f47a3faa8e88
trev3rd <trevionm17@gmail.com>
2016-05-03 16:00:22 -0400

    Added files via upload

--- ttt.py
+++ ttt.py
@@ -1,15 +1,25 @@
-from nltk.tokenize import sent_tokenize, word_tokenize
-fileName = input("Enter file name: ")
-f = open(fileName)
-raw = f.read()
+from nltk.tokenize import sent_tokenize, RegexpTokenizer #importing features from nltk library
+fileName = input("Enter file name: ")#asks user to put .txt file into cmd
+f = open(fileName,'r')
+text = f.read()
+
+
+tokenizer = RegexpTokenizer(r'\w+')#will make string only output words and not punctuation
+N=3
+acceptable_words = [x for x in tokenizer.tokenize(text) if len(x) >= N]
+average =int(len(acceptable_words)/len(sent_tokenize(text)))#dividing
+
+if text   ((".txt")):
+    print(sent_tokenize(text))#all sentnces that end with (.!?)
+    print(tokenizer.tokenize(text))#all words outputed
+    print(acceptable_words)#is words 3 letters or more
+    print('number of sentences equal',len(sent_tokenize(text)))
+    print('number of words equal',len(tokenizer.tokenize(text)))
+    print('number of acceptable words is',len(acceptable_words))
+    print('average word per senetnces eqauls',average)
+else:
+    print("Extension must be a .txt file")
 
 
 
-print(sent_tokenize(raw))
-print(word_tokenize(raw))
-print('number of sentences equal',len(sent_tokenize(raw)))
-print('number of words equal',len(word_tokenize(raw)))
 
-
-average =(len(word_tokenize(raw))/len(sent_tokenize(raw)))
-print('average word per senetnces eqauls',average)
